572b33771098056ef88919c2e0acde2a0eba21dc
Marianaga98 <93684382+Marianaga98@users.noreply.github.com>
2022-09-28 22:42:50 +0200

    Add files via upload

---
+++ Consultas_tarea_27_Sep_Mariana.sql
@@ -0,0 +1,27 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 /*CONSULTAS PARA CASA*/\
+\
+/*MARIANA GONZALEZ Alzueta*/\
+\
+\
+\
+/*Muestra el nombre del evento base junto con el g\'e9nero de los participantes de cada uno como "G\'e9nero"*/\
+\
+\
+\
+/*Muestra el nombre de cada atleta junto con la/s medalla/s que obtuvo\
+NOTA: Pueden aparecer atletas repetidos ya que algunos participan en m\'e1s de un evento*/\
+\
+\
+\
+/*Muestra las naciones y el n\'famero de atletas de cada una de ellas como "N\'famero de atletas"*/\
+\
+\
+\
+}
