e36720d8ca2812a50544143248239a60ea709112
Uo290723 <113928060+noelixu98@users.noreply.github.com>
2022-10-03 12:25:01 +0200

    ejercicios resueltos noelia

--- Consultas_tarea_27_Sep_Mariana.sql
+++ Consultas_tarea_27_Sep_Mariana.sql
@@ -1,27 +1,26 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 /*CONSULTAS PARA CASA*/\
-\
-/*MARIANA GONZALEZ Alzueta*/\
-\
-\
-\
-/*Muestra el nombre del evento base junto con el género de los participantes de cada uno como "Género"*/\
-\
-\
-\
+/*CONSULTAS PARA CASA*/
+/*SOLUCIONES NOELIA*/
+
+/*Muestra el nombre del evento base junto con el género de los participantes de cada uno como "Género"*/
+SELECT event, event_gender as "Genero"
+FROM olym.olym_events
+INNER JOIN olym.olym_base_events on olym.olym_events.base_event_id = olym.olym_events.id;v
+
 /*Muestra el nombre de cada atleta junto con la/s medalla/s que obtuvo\
-NOTA: Pueden aparecer atletas repetidos ya que algunos participan en más de un evento*/\
-\
-\
-\
-/*Muestra las naciones y el número de atletas de cada una de ellas como "Número de atletas"*/\
-\
-\
-\
-}
+NOTA: Pueden aparecer atletas repetidos ya que algunos participan en más de un evento*/
+NOTA: Pueden aparecer atletas repetidos ya que algunos participan en más de un evento*/
+SELECT  athlete_name, medal
+FROM olym.olym_athlete_games
+INNER JOIN olym.olym_athletes on olym.olym_athlete_games.athlete_id  = olym.olym_athletes.id
+INNER JOIN olym.olym_medals  on olym.olym_medals.athlete_game_id =   olym.olym_athlete_games.id;
+
+/*Muestra las naciones y el número de atletas de cada una de ellas como "Número de atletas"*/
+SELECT nation, count(athlete_id) as "numero de atletas"
+FROM olym.olym_athlete_games
+INNER JOIN olym.olym_nations on olym.olym_athlete_games.nation_id =  olym.olym_nations.id
+GROUP BY nation;
+
+
+
+
